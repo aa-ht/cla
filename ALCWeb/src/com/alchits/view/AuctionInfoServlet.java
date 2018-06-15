c928c14461a883f6f2da45168a2c230262db39ef
kavya <kavya@huhy-node18>
2018-06-15 17:55:53 +0530

    changes done in auctionInfoServlet

--- ALCWeb/src/com/alchits/view/AuctionInfoServlet.java
+++ ALCWeb/src/com/alchits/view/AuctionInfoServlet.java
@@ -40,10 +40,10 @@ public class AuctionInfoServlet extends HttpServlet {
 		String password = request.getParameter("pwd");
 		
 		if(password!=null && password.equals("alc41021080#")){	
-			<iframe width="84%" height="400px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSQn8bDfW5SxXLVftz_bfDjpG-64oQm66qwpAsR_wmOVRskbdFpqIiVa9twpvpy6H6tPFM3W0Y04Rt5/pubhtml?widget=true&amp;headers=false"></iframe>
-			<br /><br /><br />
-			<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQmC6JTtpkF1x_Hq60rJTDQlMZbD4PKAMAuzQKA_WJF388rCZxtupf25oj3PQPGHCqlPX1DIS-alkJO/embed?start=false&loop=true&delayms=5000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
-			out.println("Done");
+			out.println("<iframe width=\"84%\" height=\"400px\" src=\"https://docs.google.com/spreadsheets/d/e/2PACX-1vSQn8bDfW5SxXLVftz_bfDjpG-64oQm66qwpAsR_wmOVRskbdFpqIiVa9twpvpy6H6tPFM3W0Y04Rt5/pubhtml?widget=true&amp;headers=false\"></iframe>");
+			out.println("<br /><br /><br />");
+			out.println("<iframe src=\"https://docs.google.com/presentation/d/e/2PACX-1vQmC6JTtpkF1x_Hq60rJTDQlMZbD4PKAMAuzQKA_WJF388rCZxtupf25oj3PQPGHCqlPX1DIS-alkJO/embed?start=false&loop=true&delayms=5000\" frameborder=\"0\" width=\"960\" height=\"569\" allowfullscreen=\"true\" mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\"></iframe>");
+			
 		}
 		else{
 			out.println("Access Denied!!"); 
